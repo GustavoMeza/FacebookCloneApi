6219f52950efb13fe7619f90d1fd15200b6b1c97
GustavoMeza <coollive11@gmail.com>
2019-10-25 02:07:35 -0500

    API to CRUD users

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FacebookApi.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("firstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("lastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; }
+    }
+}
